9096aa9da7fc53a9fd77c1ad09d69f711342f052
liam liam <liam@hoo.com>
2022-05-23 00:25:47 +0800

    slice operation

--- lh-slice/main.go
+++ lh-slice/main.go
@@ -70,6 +70,34 @@ func delElement4() {
 	log.Printf("%+v", nums)
 }
 
+func operationSli() {
+	//操作slice
+	//语法 slice[n:m] 获取slice的n到m之间的所有元素(不包括m)
+	sli := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	//剪切slice
+	log.Println(sli[1:2]) // [2]
+	log.Println(sli[1:3]) // [2 3]
+	log.Println(sli[1:4]) // [2 3 4]
+	log.Println(sli[:])   //获取整个slice 是sli[0:len(sli)]的简写  [1 2 3 4 5 6 7 8 9 10]
+	log.Println(sli[:1])  // [1]
+	log.Println(sli[:2])  // [1 2]
+	log.Println(sli[:3])  // [1 2 3]
+	log.Println(sli[1:])  // [2 3 4 5 6 7 8 9 10]
+	log.Println(sli[2:])  // [3 4 5 6 7 8 9 10]
+	log.Println(sli[3:])  // [4 5 6 7 8 9 10]
+
+	//追加元素
+	log.Println(append(sli, 11, 12, 13)) // [1 2 3 4 5 6 7 8 9 10 11 12 13]
+	//合并
+	sli1 := []int{11, 12, 13}
+	log.Println(append(sli, sli1...)) // [1 2 3 4 5 6 7 8 9 10 11 12 13]
+	//复制
+	sli3 := make([]int, 10)
+	copy(sli3, sli[0:3])
+	log.Println(sli3) //[1 2 3 0 0 0 0 0 0 0]
+}
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 	delElement1()
@@ -80,4 +108,6 @@ func main() {
 	利用append 性能较差,但是容易理解
 	修改本slice的性能最好,但是复杂点
 	*/
+
+	operationSli()
 }
